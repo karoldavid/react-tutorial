396db0a01de782e14bab9dc086a7cabebfbcb1c6
karoldavid <k.zysk@zoho.com>
2017-03-05 13:41:00 +0100

    write a hello world react component 2 stateless

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello Series</h1>
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello Series</h1>
+//   }
+// }
+
+const App = () => <h1>Hello stateless</h1>
 
 export default App
