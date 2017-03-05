ad11a83a71bf91d376a2e34b489ad0e05a08a7e3
karoldavid <k.zysk@zoho.com>
2017-03-05 13:58:25 +0100

    set properties on react components 1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>Hello Series</h1>
-        <b>Bold</b>
-      </div>
-    )
+    return <h1>{this.props.txt}</h1>
   }
 }
 
